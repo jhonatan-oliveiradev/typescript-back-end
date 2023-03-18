53e806719ebe5f39555d6522551279f0e67ae584
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-03-18 16:24:48 -0300

    :construction: feat: docker img criada

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("started");
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("Server up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
